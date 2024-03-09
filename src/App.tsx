28c212bcdb587bc8819a8c220fd6921e127892b8
lokises <lokises@mcircle.biz>
2024-03-09 18:47:07 +0900

    탭 아이콘 변경, 이미지 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='logo.svg' className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={`${process.env.PUBLIC_URL}/images/logo.png`} className="App-logo" alt="logo" />
       </header>
     </div>
   );
